aa7ca0b6480f7cac0486eae7c7407171b257ccb9
rx-ted <1552488649@qq.com>
2023-06-07 22:02:19 +0800

    reset

--- main.ts
+++ main.ts
@@ -2,10 +2,34 @@
  * @Author: rx-ted
  * @Date: 2023-06-05 21:17:59
  * @LastEditors: rx-ted
- * @LastEditTime: 2023-06-05 21:18:05
+ * @LastEditTime: 2023-06-07 22:01:52
  */
 import { serve } from "https://deno.land/std@0.181.0/http/server.ts";
+import { exists } from "https://deno.land/std@0.182.0/fs/mod.ts";
 
-serve(() => {
-  return fetch(new URL("./Readme.md", import.meta.url));
+const OPENAI_API_HOST = "api.openai.com";
+
+const INDEX_URL = Deno.env.get("INDEX_URL")||"https://raw.githubusercontent.com/xqdoo00o/chatgpt-web/main/index.html"
+
+let index = '';
+
+serve(async (request) => {
+    const url = new URL(request.url);
+    url.protocol = "https";
+    const pathname = url.pathname;
+    if (pathname.startsWith('/v1/')|| pathname.startsWith('/dashboard')){
+        url.host = OPENAI_API_HOST;
+        return await fetch(url, request);
+    }
+    else{
+        if (index == '') {
+            const res = await fetch(INDEX_URL);
+            index = await res.text();
+        }
+        return new Response(index, {
+            headers: {
+                "content-type": "text/html;charset=UTF-8",
+            },
+        });
+    }
 });
